42cbf5fb803bc0a62426b0203b0bcc9abab4210b
belbeche <wbelbeche.s@gmail.com>
2024-07-18 16:53:10 +0000

    wip: config nextcloud in prod under debian 11

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc221u8d605p',
+  'passwordsalt' => 'ePHdj9o6tYykkQ3fgXk2Lix1Q3W64T',
+  'secret' => '0PYDAy+PyeR29uEtQOBmcF6yJ+4152ZRR5N64vxGgJ4K4q6Y',
+  'trusted_domains' => 
+  array (
+    0 => 'crmt.scriptzenit.fr',
+  ),
+  'datadirectory' => '/home/container/webroot/data',
+  'dbtype' => 'mysql',
+  'version' => '28.0.4.1',
+  'overwrite.cli.url' => 'http://crmt.scriptzenit.fr',
+  'dbname' => 's22_crmt_scriptzenit',
+  'dbhost' => 'node01.scriptzenit.fr:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'u22_4OvBLBs8uD',
+  'dbpassword' => 's4@a3NA76Poeww@.gVz1C.Cd',
+  'installed' => true,
+  'skeletondirectory' => '',
+  'updater.release.channel' => 'beta',
+  'updater.secret' => '$2y$10$oodPJJ7mLQVG0CuL8pk3FeWwC/KjJQHm9q6XuTD3qsspCdD3YGuLS',
+);
